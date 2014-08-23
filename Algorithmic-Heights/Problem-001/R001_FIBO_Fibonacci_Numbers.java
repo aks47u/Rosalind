aa865a97c56cdc3592054e784951f9aea227c788
Conor Fitzpatrick <aks47u@hotmail.co.uk>
2014-08-23 11:13:44 +0100

    Create R001_FIBO_Fibonacci_Numbers.java

---
+++ Algorithmic-Heights/Problem-001/R001_FIBO_Fibonacci_Numbers.java
@@ -0,0 +1,23 @@
+package Algorithmic_Heights;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class R001_FIBO_Fibonacci_Numbers {
+	public static void main(String[] args) throws FileNotFoundException {
+		Scanner scn = new Scanner(new File("rosalind_fibo.txt"));
+		int term = Integer.parseInt(scn.nextLine().trim());
+		scn.close();
+
+		int fib1 = 0, fib2 = 1, result = 0;
+
+		for (int i = 0; i < term - 1; i++) {
+			result = fib1 + fib2;
+			fib2 += fib1;
+			fib1 = fib2 - fib1;
+		}
+
+		System.out.println(result);
+	}
+}
