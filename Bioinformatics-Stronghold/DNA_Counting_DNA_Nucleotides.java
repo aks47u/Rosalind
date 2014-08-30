9bea72aec875a752277ffc9ec9f65c7f5ea6a11b
Conor Fitzpatrick <aks47u@hotmail.co.uk>
2014-08-30 10:40:10 +0100

    Create DNA_Counting_DNA_Nucleotides.java

---
+++ Bioinformatics-Stronghold/DNA_Counting_DNA_Nucleotides.java
@@ -0,0 +1,49 @@
+package Bioinformatics_Stronghold;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class DNA_Counting_DNA_Nucleotides {
+	private static ArrayList<String> inData;
+
+	public static void main(String[] args) throws IOException {
+		loadFile(new File("rosalind_dna.txt"));
+		int a = 0, c = 0, g = 0, t = 0;
+
+		for (int i = 0; i < inData.get(0).length(); i++) {
+			switch (inData.get(0).charAt(i)) {
+			case 'A':
+				a++;
+				break;
+			case 'C':
+				c++;
+				break;
+			case 'G':
+				g++;
+				break;
+			case 'T':
+				t++;
+				break;
+			default:
+				break;
+			}
+		}
+
+		System.out.println(a + " " + c + " " + g + " " + t);
+	}
+
+	private static void loadFile(File aFile) throws IOException {
+		BufferedReader input = new BufferedReader(new FileReader(aFile));
+		String line = null;
+		inData = new ArrayList<String>();
+
+		while ((line = input.readLine()) != null) {
+			inData.add(line);
+		}
+
+		input.close();
+	}
+}
